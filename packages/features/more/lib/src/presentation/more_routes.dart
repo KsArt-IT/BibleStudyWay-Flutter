88f1fe4c05b6c1294a0f94b16e276192933898fe
KsArT-IT <ksart.it@gmail.com>
2025-07-02 19:57:09 +0300

    feat(feature_more): add routes and screen to feature_more module

---
+++ packages/features/more/lib/src/presentation/more_routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:more/src/presentation/screens/more_screen.dart';
+
+abstract final class MoreRoutes {
+  /// Назва маршруту екрану додаткових функцій
+  static const String moreScreenName = 'more_screen';
+
+  /// Путь маршруту екрану додаткових функцій
+  static const String _moreScreenPath = '/more';
+
+  /// Метод для побудови вітки маршрутів по фічі
+  ///
+  /// Приймає:
+  /// - [routes] - вкладені маршрути
+  static StatefulShellBranch buildShellBranch({
+    List<RouteBase> routes = const [],
+    List<NavigatorObserver>? observers,
+  }) => StatefulShellBranch(
+    initialLocation: _moreScreenPath,
+    observers: observers,
+    routes: [
+      ...routes,
+      GoRoute(
+        path: _moreScreenPath,
+        name: moreScreenName,
+        builder: (context, state) => const MoreScreen(),
+      ),
+    ],
+  );
+}
---
+++ packages/features/more/lib/src/presentation/screens/more_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+/// {@template MoreScreen}
+/// Екран додаткових функцій
+/// {@endtemplate}
+class MoreScreen extends StatelessWidget {
+  /// {@macro MoreScreen}
+  const MoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('More Screen')),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [const Text('More Screen')],
+        ),
+      ),
+    );
+  }
+}
