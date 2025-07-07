2c2fdbfef35aa5eb2b96feb0ed418d4548831822
KsArT-IT <ksart.it@gmail.com>
2025-07-07 19:57:41 +0300

    feat(core_settings): update exported repositories and services

--- packages/core/settings/lib/settings.dart
+++ packages/core/settings/lib/settings.dart
@@ -1,6 +1,8 @@
 library;
 
-export 'src/data/service/get_storage_service.dart';
 export 'src/data/repository/local_settings_repository.dart';
+export 'src/data/repository/mock_settings_repository.dart';
+export 'src/data/service/get_storage_service.dart';
+export 'src/data/service/storage_service.dart';
 export 'src/domain/models/app_settings.dart';
 export 'src/domain/repository/settings_repository.dart';
