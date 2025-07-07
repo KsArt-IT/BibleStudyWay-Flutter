b35b6d93cb7849dbaf5a0b1eb27949373583f0f3
KsArT-IT <ksart.it@gmail.com>
2025-07-07 19:00:24 +0300

    feat(core_settings): add export of models and repositories for application customization

---
+++ packages/core/settings/lib/settings.dart
@@ -0,0 +1,6 @@
+library;
+
+export 'src/data/service/get_storage_service.dart';
+export 'src/data/repository/local_settings_repository.dart';
+export 'src/domain/models/app_settings.dart';
+export 'src/domain/repository/settings_repository.dart';
