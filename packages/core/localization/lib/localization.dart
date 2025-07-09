fb681a55b0ea5bfc9a308c121434dab72a826bca
KsArT-IT <ksart.it@gmail.com>
2025-07-09 16:27:52 +0300

    feat(core_localization): add extension to get language name by locale

--- packages/core/localization/lib/localization.dart
+++ packages/core/localization/lib/localization.dart
@@ -4,3 +4,4 @@ export 'src/l10n/app_context_ext.dart';
 export 'src/l10n/gen/app_localizations.dart';
 export 'src/l10n/localization_consumer.dart';
 export 'src/l10n/localization_notifier.dart';
+export 'src/l10n/name_locale_ext.dart';
