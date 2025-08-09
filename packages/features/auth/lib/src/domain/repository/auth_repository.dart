a65ecd186a15d2460fcea15d6962caee11098a9a
KsArT-IT <ksart.it@gmail.com>
2025-08-09 23:09:17 +0300

    feat(auth): add AuthRepository interface to work with user authentication

---
+++ packages/features/auth/lib/src/domain/repository/auth_repository.dart
@@ -0,0 +1,40 @@
+import 'package:auth/auth.dart';
+import 'package:core_common/common.dart';
+
+/// {@template auth_repository}
+/// Репозиторій до роботи з Auth.
+/// {@endtemplate}
+abstract interface class AuthRepository with DiBaseRepo {
+  /// Отримати поточного користувача
+  Future<Result<AuthUser>> getCurrentUser();
+
+  /// Увійти в систему за допомогою email та пароля
+  Future<Result<AuthUser>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  /// Зареєструватися за допомогою [email] та пароля
+  Future<Result<AuthUser>> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  /// Увійти в систему за допомогою Google
+  Future<Result<AuthUser>> signInWithGoogle();
+
+  /// Вийти із системи
+  Future<Result<void>> signOut();
+
+  /// Надіслати [email] для скидання пароля
+  Future<Result<void>> sendPasswordResetEmail(String email);
+
+  /// Оновити [email]
+  Future<Result<void>> updateEmail({required String email});
+
+  /// Оновити [name]
+  Future<Result<void>> updateName({required String name});
+
+  /// Оновити [avatar]
+  Future<Result<void>> updateAvatar({required String avatar});
+}
