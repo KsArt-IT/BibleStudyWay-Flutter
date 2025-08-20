d328836f88ad64676677cd8943bf4909464d0b52
KsArT-IT <ksart.it@gmail.com>
2025-08-20 22:49:18 +0300

    feat(auth): add AuthRoutes class for managing authentication screen routes

---
+++ packages/features/auth/lib/src/presentation/auth_routes.dart
@@ -0,0 +1,34 @@
+import 'package:auth/src/presentation/screens/login/login_screen.dart';
+import 'package:auth/src/presentation/screens/registration/registration_screen.dart';
+import 'package:auth/src/presentation/screens/reset_password/reset_password.dart';
+import 'package:go_router/go_router.dart';
+
+abstract final class AuthRoutes {
+  static const String loginScreenName = 'login_screen';
+  static const String _loginScreenPath = '/login';
+
+  static const String registrationScreenName = 'registration_screen';
+  static const String _registrationScreenPath = 'registration';
+
+  static const String resetPasswordScreenName = 'reset_password_screen';
+  static const String _resetPasswordScreenPath = 'reset_password';
+
+  static GoRoute buildRoutes({List<RouteBase> routes = const []}) => GoRoute(
+    path: _loginScreenPath,
+    name: loginScreenName,
+    builder: (context, state) => const LoginScreen(),
+    routes: [
+      if (routes.isNotEmpty) ...routes,
+      GoRoute(
+        path: _registrationScreenPath,
+        name: registrationScreenName,
+        builder: (context, state) => const RegistrationScreen(),
+      ),
+      GoRoute(
+        path: _resetPasswordScreenPath,
+        name: resetPasswordScreenName,
+        builder: (context, state) => const ResetPasswordScreen(),
+      ),
+    ],
+  );
+}
