06fae311972beb6ef74419385afaf5b747675eaa
KsArT-IT <ksart.it@gmail.com>
2025-08-20 22:52:11 +0300

    feat(auth): create auth.dart to export authentication-related modules

---
+++ packages/features/auth/lib/auth.dart
@@ -0,0 +1,10 @@
+library;
+
+export 'src/presentation/auth_routes.dart';
+export 'src/domain/entity/auth_user.dart';
+export 'src/domain/repository/auth_repository.dart';
+export 'src/data/service/auth_service.dart';
+export 'src/data/service/firebase_auth_service.dart';
+export 'src/mock/mock_firebase_auth.dart';
+export 'src/mock/mock_firebase_auth_service.dart';
+export 'src/mock/mock_firebase_auth_repository.dart';
