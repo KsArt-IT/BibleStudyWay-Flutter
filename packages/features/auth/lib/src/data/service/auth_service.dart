6e4d73299b55a61f7978225e6296975a6b0011c5
KsArT-IT <ksart.it@gmail.com>
2025-08-21 10:05:26 +0300

    feat(auth): update AuthService interface add methods for user management

--- packages/features/auth/lib/src/data/service/auth_service.dart
+++ packages/features/auth/lib/src/data/service/auth_service.dart
@@ -5,21 +5,30 @@ import 'package:firebase_auth/firebase_auth.dart';
 /// Service for working with Auth.
 /// {@endtemplate}
 abstract interface class AuthService {
+  /// Sign in with email and password.
   Future<Result<User>> signInWithEmailAndPassword({
     required String email,
     required String password,
   });
 
+  /// Sign up with email and password.
   Future<Result<User>> signUpWithEmailAndPassword({
     required String email,
     required String password,
   });
 
+  /// Send password reset email.
   Future<Result<void>> sendPasswordResetEmail(String email);
-  
+
+  /// Sign out.
   Future<Result<void>> signOut();
 
-  bool checkIsSignIn();
-
+  /// Get current user.
   Future<Result<User>> getCurrentUser();
+
+  /// Update [displayName]
+  Future<Result<void>> updateDisplayName({required String name});
+
+  /// Update [photoURL] 
+  Future<Result<void>> updatePhotoURL({required String photoURL});
 }
