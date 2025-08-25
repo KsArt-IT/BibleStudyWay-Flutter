255962c7ff9ab74670e29fa70f40ff53363e34fb
KsArT-IT <ksart.it@gmail.com>
2025-08-25 18:04:56 +0300

    feat(auth): add providers for app loading screen using Bloc

---
+++ packages/features/auth/lib/src/presentation/screens/splash/splash_provider.dart
@@ -0,0 +1,31 @@
+import 'package:auth/auth.dart';
+import 'package:auth/src/presentation/screens/splash/cubit/refresh_user_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashProviders extends StatelessWidget {
+  const SplashProviders({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            final repository = context.read<AuthRepository>();
+            return RefreshUserCubit(authRepository: repository);
+          },
+        ),
+      ],
+      child: BlocListener<RefreshUserCubit, RefreshUserState>(
+        listener: (context, state) => switch (state) {
+          RefreshUserInitial() => null,
+          RefreshUserSuccess(user: final user) => context.go('/', extra: user),
+          RefreshUserFailure() => context.go(AuthRoutes.loginScreenName),
+        },
+        child: const SplashScreen(),
+      ),
+    );
+  }
+}
