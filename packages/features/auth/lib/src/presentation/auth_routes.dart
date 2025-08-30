27007262c3b0c8e2b5c490084a66853e50242699
KsArT-IT <ksart.it@gmail.com>
2025-08-30 19:39:53 +0300

    feat(auth): update authentication routes

--- packages/features/auth/lib/src/presentation/auth_routes.dart
+++ packages/features/auth/lib/src/presentation/auth_routes.dart
@@ -1,22 +1,26 @@
+import 'package:auth/src/presentation/screens/login/login_providers.dart';
 import 'package:auth/src/presentation/screens/login/login_screen.dart';
 import 'package:auth/src/presentation/screens/registration/registration_screen.dart';
 import 'package:auth/src/presentation/screens/reset_password/reset_password.dart';
 import 'package:go_router/go_router.dart';
 
 abstract final class AuthRoutes {
+  static const String splashScreenName = 'splash_screen';
+  static const String splashScreenPath = '/splash';
+
   static const String loginScreenName = 'login_screen';
-  static const String _loginScreenPath = '/login';
+  static const String loginScreenPath = '/login';
 
   static const String registrationScreenName = 'registration_screen';
-  static const String _registrationScreenPath = 'registration';
+  static const String _registrationScreenPath = '/registration';
 
   static const String resetPasswordScreenName = 'reset_password_screen';
-  static const String _resetPasswordScreenPath = 'reset_password';
+  static const String _resetPasswordScreenPath = '/reset_password';
 
   static GoRoute buildRoutes({List<RouteBase> routes = const []}) => GoRoute(
-    path: _loginScreenPath,
+    path: loginScreenPath,
     name: loginScreenName,
-    builder: (context, state) => const LoginScreen(),
+    builder: (context, state) => const LoginProviders(child: LoginScreen()),
     routes: [
       if (routes.isNotEmpty) ...routes,
       GoRoute(
