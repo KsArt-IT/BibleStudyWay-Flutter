01dd0dde153fd77b5fcb8fcab6d1cb3fbc578b42
KsArT-IT <ksart.it@gmail.com>
2025-08-30 19:56:13 +0300

    feat(auth): add a login screen with a form to enter email and password

---
+++ packages/features/auth/lib/src/presentation/screens/login/login_screen.dart
@@ -0,0 +1,147 @@
+import 'dart:io';
+
+import 'package:auth/auth.dart';
+import 'package:auth/src/presentation/screens/login/cubit/login_cubit.dart';
+import 'package:core_localization/localization.dart';
+import 'package:core_shared/shared.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  var isEnabled = false;
+  var isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController.addListener(_verifyForm);
+    _passwordController.addListener(_verifyForm);
+  }
+
+  @override
+  void dispose() {
+    _emailController.removeListener(_verifyForm);
+    _passwordController.removeListener(_verifyForm);
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _onLogin() {
+    _setLoading(true);
+    _setEnabled(false);
+    context.read<LoginCubit>().signInWithEmailAndPassword(
+      _emailController.text,
+      _passwordController.text,
+    );
+  }
+
+  void _onLoginGoogle() {
+    _setLoading(true);
+    _setEnabled(false);
+    context.read<LoginCubit>().signInWithGoogle();
+  }
+
+  void _verifyForm() {
+    final isValid =
+        _emailController.text.trim().isNotEmpty &&
+        _emailController.text.contains('@') &&
+        _passwordController.text.trim().isNotEmpty;
+    _setEnabled(isValid);
+  }
+
+  void _setLoading(bool value) {
+    if (isLoading == value) return;
+    setState(() {
+      isLoading = value;
+    });
+  }
+
+  void _setEnabled(bool value) {
+    if (isEnabled == value) return;
+    setState(() {
+      isEnabled = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Scaffold(
+      appBar: AppBar(title: Text(l10n.login)),
+      body: BlocListener<LoginCubit, LoginState>(
+        listener: (context, state) {
+          if (state is LoginSuccess) {
+            context.go('/', extra: state.user);
+          }
+          if (state is LoginFailure) {
+            _setLoading(false);
+            _verifyForm();
+            AppSnackBar.showError(context, message: state.errorMessage);
+          }
+        },
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppTextButton(
+                    label: l10n.forgotPassword,
+                    onPressed: () => context.goNamed(AuthRoutes.resetPasswordScreenName),
+                    style: AppButtonStyle.text,
+                  ),
+                  const SizedBox(width: 16),
+                  AppTextButton(
+                    label: l10n.registration,
+                    onPressed: () => context.goNamed(AuthRoutes.registrationScreenName),
+                    style: AppButtonStyle.text,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              AppTextField(label: l10n.email, controller: _emailController),
+              const SizedBox(height: 16),
+              AppPasswordTextField(label: l10n.password, controller: _passwordController),
+              const SizedBox(height: 16),
+              AppButton(
+                label: l10n.login,
+                onPressed: _onLogin,
+                isLoading: isLoading,
+                isDisabled: !isEnabled,
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AppTextButton(label: l10n.apple, onPressed: () {}, style: AppButtonStyle.text),
+                  const SizedBox(width: 16),
+                  if (Platform.isAndroid)
+                    AppTextButton(
+                      label: l10n.google,
+                      onPressed: _onLoginGoogle,
+                      style: AppButtonStyle.text,
+                    ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
