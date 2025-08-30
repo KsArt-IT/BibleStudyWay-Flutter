92b1d5d176a9613db718969418af47d37d9fa6d3
KsArT-IT <ksart.it@gmail.com>
2025-08-30 19:54:56 +0300

    feat(auth): add LoginCubit to manage login state using Bloc

---
+++ packages/features/auth/lib/src/presentation/screens/login/cubit/login_cubit.dart
@@ -0,0 +1,37 @@
+import 'package:auth/auth.dart';
+import 'package:auth/src/domain/usecases/sign_in_with_email_and_password_use_case.dart';
+import 'package:auth/src/domain/usecases/sign_in_with_google_use_case.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final SignInWithEmailAndPasswordUseCase _signInWithEmailAndPasswordUseCase;
+  final SignInWithGoogleUseCase _signInWithGoogleUseCase;
+
+  LoginCubit({
+    required SignInWithEmailAndPasswordUseCase signInWithEmailAndPasswordUseCase,
+    required SignInWithGoogleUseCase signInWithGoogleUseCase,
+  }) : _signInWithEmailAndPasswordUseCase = signInWithEmailAndPasswordUseCase,
+       _signInWithGoogleUseCase = signInWithGoogleUseCase,
+       super(LoginInitial());
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    final result = await _signInWithEmailAndPasswordUseCase(email: email, password: password);
+
+    result.map(
+      onSuccess: (user) => emit(LoginSuccess(user: user)),
+      onFailure: (error) => emit(LoginFailure(errorMessage: error.toString())),
+    );
+  }
+
+  Future<void> signInWithGoogle() async {
+    final result = await _signInWithGoogleUseCase();
+
+    result.map(
+      onSuccess: (user) => emit(LoginSuccess(user: user)),
+      onFailure: (error) => emit(LoginFailure(errorMessage: error.toString())),
+    );
+  }
+}
