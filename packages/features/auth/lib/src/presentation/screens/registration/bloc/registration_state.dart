b2194ac364603ae7259f81ec1df2c3ac7082f023
KsArT-IT <ksart.it@gmail.com>
2025-09-01 18:06:04 +0300

    feat(auth): add registration state to manage the registration process

---
+++ packages/features/auth/lib/src/presentation/screens/registration/bloc/registration_state.dart
@@ -0,0 +1,39 @@
+part of 'registration_bloc.dart';
+
+@immutable
+final class RegistrationState extends Equatable {
+  final StateStatus status;
+  final Email email;
+  final Password password;
+  final Password passwordConfirm;
+  final String? errorMessage;
+
+  bool get isValid => email.isValid && password.isValid && password.value == passwordConfirm.value;
+
+  const RegistrationState({
+    this.status = StateStatus.initial,
+    this.email = const Email(value: ''),
+    this.password = const Password(value: ''),
+    this.passwordConfirm = const Password(value: ''),
+    this.errorMessage,
+  });
+
+  RegistrationState copyWith({
+    StateStatus? status,
+    Email? email,
+    Password? password,
+    Password? passwordConfirm,
+    String? errorMessage,
+  }) {
+    return RegistrationState(
+      status: status ?? this.status,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      passwordConfirm: passwordConfirm ?? this.passwordConfirm,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, email, password, passwordConfirm, errorMessage];
+}
