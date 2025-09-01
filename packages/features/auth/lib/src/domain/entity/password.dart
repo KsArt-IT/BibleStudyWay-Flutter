a0f27e26f3a77badc380afa921fb2642b0cac3c3
KsArT-IT <ksart.it@gmail.com>
2025-09-01 18:04:26 +0300

    feat(auth): add Password class to validate passwords

---
+++ packages/features/auth/lib/src/domain/entity/password.dart
@@ -0,0 +1,11 @@
+final class Password {
+  final String value;
+
+  const Password({required this.value});
+
+  bool get isValid => _passwordRegex.hasMatch(value);
+
+  static bool validate(String value) => _passwordRegex.hasMatch(value);
+
+  static final _passwordRegex = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9_+\-!@.$%^&*()]{8,64}$');
+}
