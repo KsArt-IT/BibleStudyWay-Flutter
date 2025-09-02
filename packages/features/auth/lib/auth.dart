444996919269b9c90cde30f9adf8f68b65ca9a15
KsArT-IT <ksart.it@gmail.com>
2025-09-02 19:55:38 +0300

    feat(auth): update exported modules for Firebase and Google Sign-In integration, add new services and screens

--- packages/features/auth/lib/auth.dart
+++ packages/features/auth/lib/auth.dart
@@ -1,10 +1,16 @@
 library;
 
-export 'src/presentation/auth_routes.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:google_sign_in/google_sign_in.dart';
+
+export 'src/data/repository/firebase_auth_repository.dart';
+export 'src/data/service/google_service.dart';
 export 'src/domain/entity/auth_user.dart';
 export 'src/domain/repository/auth_repository.dart';
-export 'src/data/service/auth_service.dart';
-export 'src/data/service/firebase_auth_service.dart';
+export 'src/domain/usecases/sign_out_use_case.dart';
 export 'src/mock/mock_firebase_auth.dart';
-export 'src/mock/mock_firebase_auth_service.dart';
 export 'src/mock/mock_firebase_auth_repository.dart';
+export 'src/mock/mock_firebase_auth_service.dart';
+export 'src/presentation/auth_routes.dart';
+export 'src/presentation/screens/splash/splash_provider.dart';
+export 'src/presentation/screens/splash/splash_screen.dart';
