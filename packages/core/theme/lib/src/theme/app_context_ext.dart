09a612cb877f11763d697356c5f55901ec436729
KsArT-IT <ksart.it@gmail.com>
2025-09-05 17:44:43 +0300

    feat(theme): add getters to get theme, color scheme and text theme in AppContextExt extension

--- packages/core/theme/lib/src/theme/app_context_ext.dart
+++ packages/core/theme/lib/src/theme/app_context_ext.dart
@@ -4,6 +4,15 @@ import 'package:provider/provider.dart';
 
 /// Клас, що реалізує розширення для контексту програми
 extension AppContextExt on BuildContext {
+  /// Геттер для отримання нотифікатора теми
+  ThemeNotifier get themeNotifier => read<ThemeNotifier>();
+
   /// Геттер для отримання теми
-  ThemeNotifier get theme => read<ThemeNotifier>();
+  ThemeData get theme => Theme.of(this);
+
+  /// Геттер для отримання кольорової схеми
+  ColorScheme get colors => Theme.of(this).colorScheme;
+
+  /// Геттер для отримання текстової теми
+  TextTheme get textTheme => Theme.of(this).textTheme;
 }
