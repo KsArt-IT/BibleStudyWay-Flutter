4ce7d30f85a7186db687ade3e91af1540a0887e1
KsArT-IT <ksart.it@gmail.com>
2025-09-05 17:45:33 +0300

    feat(shared): updated export to shared.dart, added manager for snackbar and new constants

--- packages/core/shared/lib/shared.dart
+++ packages/core/shared/lib/shared.dart
@@ -3,4 +3,7 @@ library;
 export 'src/gen/assets.gen.dart';
 export 'src/gen/fonts.gen.dart';
 export 'src/screens/error_screen.dart';
-export 'src/widgets/app_snackbar.dart';
+export 'src/manager/app_snackbar.dart';
+export 'src/manager/snack_bar_manager.dart';
+export 'src/widgets/widgets.dart';
+export 'src/constants/app_constants.dart';
