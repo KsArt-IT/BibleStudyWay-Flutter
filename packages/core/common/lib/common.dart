82ce3448a4447c5df3bb9c6e5f64c8f1d36e4c97
KsArT-IT <ksart.it@gmail.com>
2025-09-08 22:34:06 +0300

    feat(common): export add exception and entity classes in common.dart

--- packages/core/common/lib/common.dart
+++ packages/core/common/lib/common.dart
@@ -2,3 +2,6 @@ library;
 
 export 'src/di/di_base_repo.dart';
 export 'src/domain/entity/result.dart';
+export 'src/domain/entity/state_status.dart';
+export 'src/domain/exception/app_exception.dart';
+export 'src/domain/exception/safe_call_mixin.dart';
