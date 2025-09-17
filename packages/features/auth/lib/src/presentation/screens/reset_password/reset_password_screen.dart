71879b5dc2543b4aff6b5a2dca9e8ea68b4156b7
KsArT-IT <ksart.it@gmail.com>
2025-09-17 21:12:38 +0300

    feat(auth): implement ResetPasswordScreen for user password reset functionality

---
+++ packages/features/auth/lib/src/presentation/screens/reset_password/reset_password_screen.dart
@@ -0,0 +1,86 @@
+import 'package:auth/src/domain/entity/email.dart';
+import 'package:auth/src/presentation/screens/reset_password/cubit/reset_password_cubit.dart';
+import 'package:core_localization/localization.dart';
+import 'package:core_shared/shared.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+class ResetPasswordScreen extends StatefulWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  State<ResetPasswordScreen> createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  final _emailController = TextEditingController();
+  var _isEnabled = false;
+
+  @override
+  void initState() {
+    _emailController.addListener(_validateEmail);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.removeListener(_validateEmail);
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  void _validateEmail() {
+    final email = _emailController.text;
+    setState(() {
+      _isEnabled = Email.validate(email);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(l10n.resetPasswordTitle),
+        centerTitle: true,
+      ),
+      body: BlocListener<ResetPasswordCubit, ResetPasswordState>(
+        listener: (context, state) {
+          if (state is ResetPasswordSuccess) {
+            AppSnackBar.showInfo(context, message: l10n.resetPasswordSuccess);
+            context.go('/');
+          }
+          if (state is ResetPasswordFailure) {
+            AppSnackBar.showError(context, message: state.errorMessage);
+          }
+        },
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                AppTextField(
+                  controller: _emailController,
+                  label: l10n.email,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 16),
+                AppButton(
+                  label: l10n.send,
+                  onPressed: () {
+                    context.read<ResetPasswordCubit>().sendPasswordResetEmail(
+                      _emailController.text,
+                    );
+                  },
+                  isDisabled: !_isEnabled,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
