57b67ca68455f119779508341455939b05f2de6a
KsArT-IT <ksart.it@gmail.com>
2025-09-19 23:46:17 +0300

    feat(shared): create widgets.dart to export button and text field components

---
+++ packages/core/shared/lib/src/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export 'buttons/app_button.dart';
+export 'buttons/app_button_style.dart';
+export 'buttons/app_text_button.dart';
+export 'text_field/app_password_text_field.dart';
+export 'text_field/app_text_field.dart';
